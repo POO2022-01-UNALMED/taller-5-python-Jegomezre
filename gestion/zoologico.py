b0169c43a645fee8bc7196b707ee6d4f246d89bb
Jeronimo Gomez Restrepo <jegomezre@unal.edu.co>
2022-04-26 22:37:30 -0500

    gestion

---
+++ gestion/zoologico.py
@@ -0,0 +1,29 @@
+class Zoologico():
+
+    def __init__(self, nombre = None, ubicacion = None):
+        self._nombre = nombre
+        self._ubicacion = ubicacion
+        self._zonas = []
+
+    def cantidadTotalAnimales(self):
+        valor = 0
+        for zona in self._zonas:
+            valor += zona.cantidadAnimales()
+        return valor 
+        
+    def agregarZonas(self, zona):
+        self._zonas.append(zona)
+
+
+    def setNombre(self, nombre):
+        self._nombre = nombre
+    def setUbicacion(self, ubicacion):
+        self._ubicacion = ubicacion   
+    def setZonas(self, zonas):
+        self._zonas = zonas
+    def getNombre(self):
+        return self._nombre
+    def getUbicacion(self):
+        return self._ubicacion
+    def getZona(self):
+        return self._zonas
